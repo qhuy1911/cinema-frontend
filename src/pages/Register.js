2a7df0255473eec8f6920a10bc15333a9059f65d
qhuy1911 <qhuy1911@gmail.com>
2022-06-19 12:32:41 +0700

    update Register Page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useRef, useEffect } from "react";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
