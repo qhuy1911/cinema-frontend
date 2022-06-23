2e98aff9f89442d3c4a9be23a4f08fc0678e611b
Phương Thảo <18130220@st.hcmuaf.edu.vn>
2022-06-23 16:18:19 +0700

    create amdin bookings
    create amdin bookings

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -3,9 +3,14 @@ import http from "./http-common";
 const createBooking = (userId) => {
     return http.post(`/user/${userId}/bookings`,{});
   };
-const BookingService = {
-    createBooking
+  const getAll = () => {
+    return http.get("/bookings");
   };
-
+  
+const BookingService = {
+    createBooking,
+    getAll
+  };
+  
 export default BookingService;
   
